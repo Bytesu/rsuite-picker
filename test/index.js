3195800f9ea473933decc4645558e15310095e55
simonguo <simonguo.2009@gmail.com>
2016-10-19 17:43:07 +0800

    Update test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,2 @@
-const srcContext = require.context('../src', true, /.*\.js$/);
-srcContext.keys().forEach(srcContext);
 
-const testsContext = require.context('.', true, /Spec$/);
-testsContext.keys().forEach(testsContext);
+require('./OptionsSpec.js');
