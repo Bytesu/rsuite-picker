4f174c45bff3018091dc3d863a4919631f91e79f
simonguo <simonguo.2009@gmail.com>
2016-10-19 17:18:44 +0800

    Update test

--- test/index.js
+++ test/index.js
@@ -1,3 +1,5 @@
+const srcContext = require.context('../src', true, /.*\.js$/);
+srcContext.keys().forEach(srcContext);
 
 const testsContext = require.context('.', true, /Spec$/);
 testsContext.keys().forEach(testsContext);
